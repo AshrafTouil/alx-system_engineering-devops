55c9fe0f4cad02abaa00de7088f3643a1c034800
Achraf <touilashraf6@gmail.com>
2024-01-26 03:03:24 -0800

    rr

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Installs flask from pip3
+# 1-install_a_package.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
+
